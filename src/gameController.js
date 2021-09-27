48b90af3135f74daff2343350dc3c1ba5bc9773f
koraytugay <ktugay@sonatype.com>
2021-09-26 21:30:55 -0400

    initial

---
+++ src/gameController.js
@@ -0,0 +1,59 @@
+import {HangmanGame, isGameOver, isGameWon, getBadGuessCount, getTargetWordMasked} from './hangmanService.js'
+
+let hangmanGame = new HangmanGame();
+redrawUi();
+
+document.querySelector('#new-game-button').addEventListener('click', function() {
+  hangmanGame = new HangmanGame();
+  redrawUi();
+});
+
+document.addEventListener('keydown', function({key}) {
+  if ([...'abcdefghijklmnopqrstuvwxyz'].includes(key)) {
+    hangmanGame.attempt(key);
+    redrawUi();
+  }
+});
+
+function redrawUi() {
+  drawSecretWord();
+  drawHangmanImage();
+  drawLetters();
+}
+
+function drawSecretWord() {
+  const secretWordContainer = document.querySelector('#secret-word-container');
+  secretWordContainer.innerHTML = '';
+
+  for (const letter of isGameOver(hangmanGame) ? hangmanGame.targetWord : getTargetWordMasked(hangmanGame)) {
+    const targetLetterSpan = document.createElement('span');
+    targetLetterSpan.innerText = letter;
+    secretWordContainer.appendChild(targetLetterSpan);
+  }
+}
+
+function drawHangmanImage() {
+  let images = `resources/images/`;
+  Object.assign(document.querySelector("#hangman-image"), {
+    src: isGameWon(hangmanGame) ? `${images}youwin.png` : `${images}${getBadGuessCount(hangmanGame)}.png`
+  });
+}
+
+function drawLetters() {
+  const lettersContainer = document.querySelector('#letters-container');
+  lettersContainer.innerHTML = '';
+
+  for (let letter of [...'abcdefghijklmnopqrstuvwxyz']) {
+    const letterButton = document.createElement('button');
+    letterButton.classList.add('guess-letter');
+    Object.assign(letterButton, {
+      textContent: letter,
+      disabled: isGameOver(hangmanGame) || isGameWon(hangmanGame) || hangmanGame.attempts.includes(letter)
+    });
+    letterButton.addEventListener('click', () => {
+      hangmanGame.attempt(letter);
+      redrawUi();
+    });
+    lettersContainer.appendChild(letterButton);
+  }
+}
---
+++ src/hangmanService.js
@@ -0,0 +1,50 @@
+import words from './words.js';
+
+export function HangmanGame() {
+  this.targetWord = words[Math.floor(Math.random() * words.length)];
+  this.attempts = [];
+  this.attempt = function(letter) {
+    if (!this.attempts.includes(letter)) {
+      this.attempts.push(letter);
+    }
+  }
+  console.log(this.targetWord);
+}
+
+export function getBadGuessCount({targetWord, attempts}) {
+  let badGuessCount = 0;
+  for (const attempt of attempts) {
+    if (![...new Set(targetWord)].includes(attempt)) {
+      badGuessCount++;
+    }
+  }
+  return badGuessCount;
+}
+
+export function isGameOver({targetWord, attempts}) {
+  return getBadGuessCount({targetWord, attempts}) === 10;
+}
+
+export function isGameWon({targetWord, attempts}) {
+  let gameWon = true;
+  const targetLetters = [...new Set(targetWord)];
+  for (const targetLetter of targetLetters) {
+    if (!attempts.includes(targetLetter)) {
+      gameWon = false;
+    }
+  }
+  return gameWon;
+}
+
+export function getTargetWordMasked({targetWord, attempts}) {
+  let targetWordMasked = "";
+  for (let i = 0; i < targetWord.length; i++) {
+    if (attempts.includes(targetWord[i])) {
+      targetWordMasked = targetWordMasked.concat(targetWord[i]);
+    }
+    else {
+      targetWordMasked = targetWordMasked.concat('_');
+    }
+  }
+  return targetWordMasked;
+}
